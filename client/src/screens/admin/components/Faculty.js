c64d2fd37efa2bbbe048bf55ecf4bfbb60e53b0f
asad tabi <bcsf18a5172pucit.edu.pk>
2021-10-23 17:59:01 +0500

    Admin components completed

--- client/src/screens/admin/components/Faculty.js
+++ client/src/screens/admin/components/Faculty.js
@@ -1,7 +1,86 @@
 function Faculty() {
     return(
         <>
-            <p>Faculty Works</p>
+            <div className="col-md-11 table-container box-shadow" align="center">
+                <div className='d-flex justify-content-between align-items-center'>
+                    <div className="schedule-title">
+                        Faculty
+                    </div>
+                    <div>
+                        <button className="custom-btn background-green">Create</button>
+                    </div>
+                </div>
+                <div className="table-responsive mt-4">
+                  <table className="table table-responsive">
+                    <thead className="box-shadow-bottom">
+                    <tr>
+                        <th scope="col">Name</th>
+                        <th scope="col">Designation</th>
+                        <th scope="col">E-mail</th>
+                        <th scope="col">Contact</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr className="table-data">
+                        <td>Mam Esha Aftab</td>
+                        <td>Assistant Professor</td>
+                        <td>Esha.Aftab@pucit.edu.pk</td>
+                        <td>03xx-xxxxxxx</td>
+                    </tr>
+                    <tr className="table-data">
+                        <td>Mam Esha Aftab</td>
+                        <td>Assistant Professor</td>
+                        <td>Esha.Aftab@pucit.edu.pk</td>
+                        <td>03xx-xxxxxxx</td>
+                    </tr> 
+                    <tr className="table-data">
+                        <td>Mam Esha Aftab</td>
+                        <td>Assistant Professor</td>
+                        <td>Esha.Aftab@pucit.edu.pk</td>
+                        <td>03xx-xxxxxxx</td>
+                    </tr>
+                     <tr className="table-data">
+                        <td>Mam Esha Aftab</td>
+                        <td>Assistant Professor</td>
+                        <td>Esha.Aftab@pucit.edu.pk</td>
+                        <td>03xx-xxxxxxx</td>
+                    </tr>
+                     <tr className="table-data">
+                        <td>Mam Esha Aftab</td>
+                        <td>Assistant Professor</td>
+                        <td>Esha.Aftab@pucit.edu.pk</td>
+                        <td>03xx-xxxxxxx</td>
+                    </tr> 
+                    <tr className="table-data">
+                        <td>Mam Esha Aftab</td>
+                        <td>Assistant Professor</td>
+                        <td>Esha.Aftab@pucit.edu.pk</td>
+                        <td>03xx-xxxxxxx</td>
+                    </tr>
+                     <tr className="table-data">
+                        <td>Mam Esha Aftab</td>
+                        <td>Assistant Professor</td>
+                        <td>Esha.Aftab@pucit.edu.pk</td>
+                        <td>03xx-xxxxxxx</td>
+                    </tr>
+                     <tr className="table-data">
+                        <td>Mam Esha Aftab</td>
+                        <td>Assistant Professor</td>
+                        <td>Esha.Aftab@pucit.edu.pk</td>
+                        <td>03xx-xxxxxxx</td>
+                    </tr>
+                </tbody>
+                </table>
+                </div>
+                <div className='d-flex justify-content-center align-items-center pl-2'>
+                    <div>
+                        <button disabled className="custom-btn disabled me-4">Prev</button>
+                    </div>
+                    <div>
+                        <button className="custom-btn background-blue">Next</button>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
