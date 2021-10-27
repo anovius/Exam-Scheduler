4a61f95d48c1554e1f85b4adc94c28fe75df1ee2
junaid zubair <junaidzubair376@gmail.com>
2021-10-28 02:15:39 +0500

    filter modal done

--- client/src/screens/admin/components/Schedule.js
+++ client/src/screens/admin/components/Schedule.js
@@ -1,6 +1,63 @@
 function Schedule() {
     return(
         <>
+            <div className="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog" role="document">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="exampleModalLabel">Filters</h5>
+                        </div>
+                        <div className="modal-body">
+                            <div className='d-flex justify-content-left '>
+                                <div className="form-check me-4">
+                                    <input className="form-check-input" type="radio" name="degree" ></input>
+                                    <label className="form-check-label" for="flexRadioDefault1">
+                                        CS
+                                    </label>
+                                </div>
+                                <div className="form-check me-4" >
+                                    <input className="form-check-input" type="radio" name="degree" id="flexRadioDefault2" checked></input>
+                                    <label className="form-check-label" for="flexRadioDefault2">
+                                        SE
+                                    </label>
+                                </div>
+                                <div className="form-check me-4">
+                                    <input className="form-check-input" type="radio" name="degree" id="flexRadioDefault2"></input>
+                                    <label className="form-check-label" for="flexRadioDefault2">
+                                        IT
+                                    </label>
+                                </div>
+                            </div>
+                            <div className='d-flex justify-content-left mt-4'>
+                                <div className="form-check me-4">
+                                    <input className="form-check-input" type="radio" name="section" ></input>
+                                    <label className="form-check-label" for="flexRadioDefault1">
+                                        Morning
+                                    </label>
+                                </div>
+                                <div className="form-check me-4 mb-2" >
+                                    <input className="form-check-input" type="radio" name="section" id="flexRadioDefault2" checked></input>
+                                    <label className="form-check-label" for="flexRadioDefault2">
+                                        Afternoon
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="col-sm-4 ">
+                                <select class="form-select" aria-label="Default select example">
+                                    <option selected>select year</option>
+                                    <option value="2018">2018</option>
+                                    <option value="2019">2019</option>
+                                    <option value="2020">2020</option>
+                                    <option value="2021">2021</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="custom-btn background-blue" data-dismiss="modal">Apply</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <div className="col-md-11 table-container box-shadow" align="center">
                 <div className='d-flex justify-content-between align-items-center'>
                     <div className="schedule-date">
@@ -17,7 +74,7 @@ function Schedule() {
                         BSCS Session Fall 2018
                     </div>
                     <div>
-                        <button className="custom-btn background-blue ">Filter</button>
+                        <button className="custom-btn background-blue" data-toggle="modal" data-target="#exampleModal">Filter</button>
                     </div>
                 </div>
                 <div className="custom-table">
