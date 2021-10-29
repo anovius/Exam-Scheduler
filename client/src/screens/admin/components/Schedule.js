02d7a3f9e1ca682c4a7da823cde6a1a165a83918
Usman Farooq <anovius10@gmail.com>
2021-10-29 11:24:08 +0500

    design fixes

--- client/src/screens/admin/components/Schedule.js
+++ client/src/screens/admin/components/Schedule.js
@@ -44,7 +44,7 @@ function Schedule() {
                             </div>
                             <div class="col-sm-4 ">
                                 <select class="form-select" aria-label="Default select example">
-                                    <option selected>select year</option>
+                                    <option selected>Select year</option>
                                     <option value="2018">2018</option>
                                     <option value="2019">2019</option>
                                     <option value="2020">2020</option>
