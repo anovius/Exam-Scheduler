1806f556af6c70e4c164789ef36d979f3c7a401f
junaid zubair <junaidzubair376@gmail.com>
2021-10-31 19:01:37 +0500

    add student done

--- client/src/screens/admin/components/Records/AddStudent.js
+++ client/src/screens/admin/components/Records/AddStudent.js
@@ -8,6 +8,23 @@ function AddStudent(){
                 <div className="add-form">
                     <form>  
                     
+                    <div className="form-group">
+                        <label> <i className="fas fa-user"></i> Name</label><br/>
+                        <input type="text" placeholder="eg: Junaid Zubair" />
+                    </div>  
+                    <div className="form-group">
+                        <label> <i className="fas fa-clipboard-list"></i> Roll Number</label><br/>
+                        <input type="text" placeholder="eg: BCSF18A511" />
+                    </div>  
+                    <div className="form-group">
+                        <label> <i className="fas fa-envelope"></i> Email</label><br/>
+                        <input type="email" placeholder="eg: bcsf18a511@pucit.edu.pk" />
+                    </div>
+                    <div className="form-group">
+                        <label> <i className="fas fa-users-class"></i> Class</label><br/>
+                        <input type="text" placeholder="eg: BCS Afternoon F18" />
+                    </div>
+                    
                     <button type="button" className="submit background-green mt-4 mb-3"> Add Student</button> 
                     </form>
                 </div>
