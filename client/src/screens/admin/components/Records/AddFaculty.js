9fabe2800221a6d47711301229f4a18632120334
junaid zubair <junaidzubair376@gmail.com>
2021-10-31 19:27:59 +0500

    add faculty done

--- client/src/screens/admin/components/Records/AddFaculty.js
+++ client/src/screens/admin/components/Records/AddFaculty.js
@@ -7,7 +7,19 @@ function AddFaculty(){
                 <hr/>
                 <div className="add-form">
                     <form>  
-                    
+                    <div className="form-group">
+                        <label> <i className="fas fa-user"></i> Name</label><br/>
+                        <input type="text" placeholder="eg: Junaid Zubair" />
+                    </div>
+                    <div className="form-group">
+                        <label> <i className="fas fa-envelope"></i> Email</label><br/>
+                        <input type="email" placeholder="eg: faculty@pucit.edu.pk" />
+                    </div>
+                    <div className="form-group">
+                        <label> <i className="fas fa-phone-alt"></i> Phone</label><br/>
+                        <input type="text" placeholder="eg: 0321-2312314" />
+                    </div>  
+
                     <button type="button" className="submit background-green mt-4 mb-3"> Add Faculty</button> 
                     </form>
                 </div>
