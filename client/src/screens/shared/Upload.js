36325e8524db3933ffdf03a39b7944fa69f917db
Usman Farooq <anovius10@gmail.com>
2021-12-12 02:20:30 +0500

    uploader implemented

---
+++ client/src/screens/shared/Upload.js
@@ -0,0 +1,19 @@
+import React, {useCallback} from 'react'
+import {useDropzone} from 'react-dropzone'
+
+import './shared.css'
+
+function Upload() {
+  const onDrop = useCallback(acceptedFiles => {
+    // Do something with the files
+  }, [])
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+
+  return (
+    <div {...getRootProps()} className='file-drop'>
+      <input {...getInputProps()} />
+      <p className='mt-3'>Drag and drop or select file</p>
+    </div>
+  )
+}
+export default Upload;
