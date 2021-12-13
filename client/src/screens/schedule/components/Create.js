83b0e0ba2cd68ea482e4198353426a2893121823
Usman Farooq <anovius10@gmail.com>
2021-12-13 18:56:52 +0500

    stepper

--- client/src/screens/schedule/components/Create.js
+++ client/src/screens/schedule/components/Create.js
@@ -6,8 +6,29 @@ function Create(){
     return(
         <>
             <div className="schedule-container">
-                <div className="col-md-6 wizard box-shadow">
-                    <div className="main">
+                <div className="col-md-6 col-12 wizard box-shadow d-flex align-items-center">
+                    <div className="main col-md-8">
+                        <form>
+                            <div class="row mx-0 mb-3">
+                            <div class="col">
+                                <label class="form-label">Title</label>
+                                <input type="text" class="form-control" placeholder="Title"/>
+                            </div>
+                            </div>
+                            <div class="row mx-0">
+                            <div class="col">
+                                <label class="form-label">Title</label>
+                                <input type="text" class="form-control" placeholder="Starting Date"/>
+                            </div>
+                            <div class="col">
+                                <label class="form-label">Title</label>
+                                <input type="text" class="form-control" placeholder="Ending Date"/>
+                            </div>
+                            </div>
+                        </form>
+                    </div>
+                    <div className="col-md-4">
+                       
                     </div>
                     <div className="navigation">
                         { stepNumber !== 0 && <button className="custom-btn me-3 text-black-50" onClick={() => setStepNumber(stepNumber - 1)}>Previous</button>}
