22d219a151e32396a540be2d35b90c8a59218f01
Usman Farooq <anovius10@gmail.com>
2021-12-17 05:10:57 +0500

    fixes

--- client/src/screens/schedule/components/Edit.js
+++ client/src/screens/schedule/components/Edit.js
@@ -1,9 +1,33 @@
-// import { ScheduleComponent, Day, Week, Inject, ViewsDirective, ViewDirective } from '@syncfusion/ej2-react-schedule';
+import { Calendar, momentLocalizer  } from 'react-big-calendar' 
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import moment from 'moment'
+const localizer = momentLocalizer(moment)
 
 function Edit(){
+    const events = [
+        {
+            title: 'Programming Fundamentals',
+            start: new Date(), // year month day hour minute second
+            end: new Date(),
+            allDay: true
+        },
+        {
+            title: 'Data Structures',
+            start: new Date(),
+            end: new Date(),
+            allDay: true
+        }
+    ];
     return (
         <>
-            
+            <Calendar
+                defaultView='week'
+                localizer={localizer}
+                events={events}
+                startAccessor="start"
+                endAccessor="end"
+                tooltipAccessor="title"
+            />
         </>
     )
 }
