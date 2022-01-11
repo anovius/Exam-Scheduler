2a5598b04947a16c98922fab0ffa267a17542834
Usman Farooq <anovius10@gmail.com>
2022-01-12 00:57:59 +0500

    reducers added

---
+++ client/src/store/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit';
+import userReducer from './reducer/userSlice';
+
+export default configureStore({
+  reducer: {
+    user: userReducer,
+  },
+});
