896889035afd7cff5bdb264e6e1164e1baecce8d
Usman Farooq <anovius10@gmail.com>
2022-06-12 14:51:42 -0700

    all done on front side for creating schedual

--- client/src/screens/schedule/components/Create.js
+++ client/src/screens/schedule/components/Create.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
 
 import logo from "../../../assets/logo-dark.png";
 import SubjectService from "../../../store/action/subject.service";
@@ -7,19 +8,77 @@ import UserService from "../../../store/action/user.service";
 
 function Create(){
     const [stepNumber, setStepNumber] = useState(0);
-
     const [subjects, setSubjects] = useState([]);
     const [teachers, setTeachers] = useState([]);
     const [rooms, setRooms] = useState([]);
     const [slots, setSlots] = useState([]);
     const [priorityShow, setPriorityShow] = useState(false);
+    const [selectedIndex, setSelectedIndex] = useState(0);
+    const [selectedTeacher, setSelectedTeacher] = useState(null);
+    const [selectedRoom, setSelectedRoom] = useState(null);
+    const [selectedSlot, setSelectedSlot] = useState(null);
+    const [selectedDate, setSelectedDate] = useState(null);
+    const [title, setTitle] = useState(null);
+    const [start, setStart] = useState(null);
+    const [end, setEnd] = useState(null);
 
     const handleSubjectClick = (index) => {
+        setSelectedIndex(index);
         setSubjects(
             subjects.map((subject, i) => 
                 i === index ? {...subject, isSelected: !subject.isSelected} : subject
             )
         );
+    }
+
+
+    const handleCheck = () => {
+        let check = subjects.find(subject => subject.teacher === selectedTeacher && subject.slot === selectedSlot && subject.date === selectedDate);
+        if(check){
+            Swal.fire({
+                title: "Error",
+                text: "Conflict with same teacher, date and slot",
+                icon: "error",
+                confirmButtonText: "OK"
+            });
+            return false;
+        }
+
+        let check2 = subjects.find(subject => subject.room === selectedRoom && subject.date === selectedDate && subject.slot === selectedSlot);
+        if(check2){
+            Swal.fire({
+                title: "Error",
+                text: "Selected Room is not Available",
+                icon: "error",
+                confirmButtonText: "OK"
+            });
+            return false;
+        }
+
+        return true;
+
+    }
+
+    const handleSet = () => {
+        if(!handleCheck()){
+            return;
+        }
+        if(selectedTeacher && selectedRoom && selectedSlot && selectedDate){
+            setPriorityShow(false)
+            setSubjects(
+                subjects.map((subject, i) =>
+                    i === selectedIndex ? {...subject, room: selectedRoom, teacher: selectedTeacher, slot: selectedSlot, date: selectedDate} : subject
+                )
+            );
+
+            setSelectedTeacher(null);
+            setSelectedRoom(null);
+            setSelectedSlot(null);
+            setSelectedDate(null);
+        }
+        else{
+            return;
+        }
     }
 
     useEffect(() => {
@@ -29,10 +88,11 @@ function Create(){
             res.data.data.forEach(subject => {
                 data.push({
                     name: subject.name,
-                    isSelectedL: false,
+                    isSelected: false,
                     room: "",
                     date: "",
                     slot: "",
+                    teacher: "",
                 })
             })
             setSubjects(data);
@@ -59,13 +119,13 @@ function Create(){
                     <h4 className="text-left">Fill Details</h4>
                     <div className="row">
                         <div className="col">
-                            <select class="form-control">
+                            <select class="form-control" onChange={(e) => {setSelectedTeacher(e.target.value)}}>
                                 <option selected>Select Teacher</option>
                                 {teachers.map(teacher => { return <option>{teacher.fullName}</option> })}
                             </select>
                         </div>
                         <div className="col">
-                            <select class="form-control">
+                            <select class="form-control" onChange={(e) => {setSelectedRoom(e.target.value)}}>
                                 <option selected>Select Room</option>
                                 {rooms.map(room => { return <option>{room.name}</option> })}
                             </select>
@@ -73,19 +133,19 @@ function Create(){
                     </div>
                     <div className="row mt-4">
                         <div className="col">
-                            <select class="form-control">
+                            <select class="form-control" onChange={(e) => {setSelectedSlot(e.target.value)}}>
                                 <option selected>Select Slot</option>
                                 {slots.map(slot => { return <option>{slot.start} - {slot.end}</option> })}
                             </select>
                         </div>
                         <div className="col">
                             <div class="form-group">
-                                <input type="text" class="form-control-file" onFocus={(e) => (e.currentTarget.type = "date")} placeholder="Select Date"/>
+                                <input type="text" class="form-control-file" onFocus={(e) => (e.currentTarget.type = "date")} onChange={(e) => {setSelectedDate(e.target.value)}} min={start} max={end} placeholder="Select Date"/>
                             </div>
                         </div>
                     </div>
                     <div className="d-flex flex-row-reverse mt-5">
-                        <button className="custom-btn background-blue" onClick={() => setPriorityShow(false)}>Set</button>
+                        <button className="custom-btn background-blue" onClick={handleSet}>Set</button>
                     </div>
                 </div>
             </div> }
@@ -99,17 +159,17 @@ function Create(){
                                 <div class="row mx-0 mb-3">
                                     <div class="col">
                                         <label class="form-label">Title</label>
-                                        <input type="text" class="form-control" placeholder="e.g. Mid Term Fall 19"/>
+                                        <input type="text" class="form-control" placeholder="e.g. Mid Term Fall 19" onChange={(e) => setTitle(e.target.value)} />
                                     </div>
                                 </div>
                                 <div class="row mx-0">
                                     <div class="col">
                                         <label class="form-label">Starting Date</label>
-                                        <input type="date" class="form-control"/>
+                                        <input type="date" class="form-control"  onChange={(e) => setStart(e.target.value)} />
                                     </div>
                                     <div class="col">
                                         <label class="form-label">Ending Date</label>
-                                        <input type="date" class="form-control"/>
+                                        <input type="date" class="form-control"  onChange={(e) => setEnd(e.target.value)} />
                                     </div>
                                 </div>
                             </div> }
@@ -127,13 +187,10 @@ function Create(){
                             
                         </form>
                     </div>
-                    <div className="col-md-4">
-                       
-                    </div>
                     <div className="navigation">
                         { stepNumber !== 0 && <button className="custom-btn me-3 text-black-50" onClick={() => setStepNumber(stepNumber - 1)}>Previous</button>}
-                        { stepNumber !== 2 && <button className="custom-btn background-blue" onClick={() => setStepNumber(stepNumber + 1)} >Next</button>}
-                        { stepNumber === 2 && <Link to="/admin"><button className="custom-btn background-blue">Finish</button></Link>}
+                        { stepNumber !== 1 && <button className="custom-btn background-blue" onClick={() => setStepNumber(stepNumber + 1)} >Next</button>}
+                        { stepNumber === 1 && <Link to="/admin"><button className="custom-btn background-blue">Finish</button></Link>}
                     </div>
                 </div>
             </div>
