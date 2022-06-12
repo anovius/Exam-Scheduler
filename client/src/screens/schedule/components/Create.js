c34bc0182cfbc7ae5b83d8eb50fd8758342bd153
Usman Farooq <anovius10@gmail.com>
2022-06-12 13:52:13 -0700

    getting live things in creating scedual

--- client/src/screens/schedule/components/Create.js
+++ client/src/screens/schedule/components/Create.js
@@ -1,129 +1,94 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 import logo from "../../../assets/logo-dark.png";
+import SubjectService from "../../../store/action/subject.service";
+import UserService from "../../../store/action/user.service";
 
 function Create(){
     const [stepNumber, setStepNumber] = useState(0);
-    var subjects = [
-        {
-            name: "PF",
-            isSelected: false
-        },
-        {
-            name: "COAL",
-            isSelected: false
-        },
-        {
-            name: "OOP",
-            isSelected: false
-        },
-        {
-            name: "DSA",
-            isSelected: false
-        },
-        {
-            name: "DBMS",
-            isSelected: false
-        },
-        {
-            name: "OS",
-            isSelected: false
-        },
-        {
-            name: "SP",
-            isSelected: false
-        },
-        {
-            name: "CA",
-            isSelected: false
-        },
-        {
-            name: "AOA",
-            isSelected: false
-        },
-        {
-            name: "TOA",
-            isSelected: false
-        },
-        {
-            name: "Compiler",
-            isSelected: false
-        },
-        {
-            name: "PM",
-            isSelected: false
-        },
-        {
-            name: "TBW",
-            isSelected: false
-        },
 
-    ];
-
-    const [subjectsSelected, setSubjectsSelected] = useState(subjects);
+    const [subjects, setSubjects] = useState([]);
+    const [teachers, setTeachers] = useState([]);
+    const [rooms, setRooms] = useState([]);
+    const [slots, setSlots] = useState([]);
+    const [priorityShow, setPriorityShow] = useState(false);
 
     const handleSubjectClick = (index) => {
-        setSubjectsSelected(
-            subjectsSelected.map((subject, i) => 
+        setSubjects(
+            subjects.map((subject, i) => 
                 i === index ? {...subject, isSelected: !subject.isSelected} : subject
             )
         );
     }
 
-    var teachers = [
-        {
-            name: "Abc",
-            isSelected: false
-        },
-        {
-            name: "Lorem",
-            isSelected: false
-        },
-        {
-            name: "Ipsum",
-            isSelected: false
-        },
-        {
-            name: "Xyz",
-            isSelected: false
-        },
-        {
-            name: "JKL",
-            isSelected: false
-        },{
-            name: "Abc",
-            isSelected: false
-        },
-        {
-            name: "Lorem",
-            isSelected: false
-        },
-    ];
+    useEffect(() => {
+        SubjectService.getSubjects().then(res => {
+            let data = [];
 
-    const [teachersSelected, setTeachersSelected] = useState(teachers);
+            res.data.data.forEach(subject => {
+                data.push({
+                    name: subject.name,
+                    isSelectedL: false,
+                    room: "",
+                    date: "",
+                    slot: "",
+                })
+            })
+            setSubjects(data);
+        });
 
-    const handleTeacherClick = (index) => {
-        setTeachersSelected(
-            teachersSelected.map((teacher, i) => 
-                i === index ? {...teacher, isSelected: !teacher.isSelected} : teacher
-            )
-        );
-    }
+        UserService.getRooms().then(res => {
+            setRooms(res.data.data);
+        })
 
-    const [priorityShow, setPriorityShow] = useState(false);
+        UserService.getSlots().then(res => {
+            setSlots(res.data.data);
+        })
+
+        UserService.getTeachers().then(res => {
+            setTeachers(res.data.data);
+        })
+    }, [])
+
 
     return(
         <>
             { priorityShow && <div className="priority box-shadow">
                 <div className="col-md-10 mt-3">
-                    <h4 className="text-center">Select Priority</h4>
-                    <input type="range" class="form-range" min="1" max="6"></input>
+                    <h4 className="text-left">Fill Details</h4>
+                    <div className="row">
+                        <div className="col">
+                            <select class="form-control">
+                                <option selected>Select Teacher</option>
+                                {teachers.map(teacher => { return <option>{teacher.fullName}</option> })}
+                            </select>
+                        </div>
+                        <div className="col">
+                            <select class="form-control">
+                                <option selected>Select Room</option>
+                                {rooms.map(room => { return <option>{room.name}</option> })}
+                            </select>
+                        </div>
+                    </div>
+                    <div className="row mt-4">
+                        <div className="col">
+                            <select class="form-control">
+                                <option selected>Select Slot</option>
+                                {slots.map(slot => { return <option>{slot.start} - {slot.end}</option> })}
+                            </select>
+                        </div>
+                        <div className="col">
+                            <div class="form-group">
+                                <input type="text" class="form-control-file" onFocus={(e) => (e.currentTarget.type = "date")} placeholder="Select Date"/>
+                            </div>
+                        </div>
+                    </div>
                     <div className="d-flex flex-row-reverse mt-5">
                         <button className="custom-btn background-blue" onClick={() => setPriorityShow(false)}>Set</button>
                     </div>
                 </div>
-            </div>}
+            </div> }
             <div className="schedule-container">
                 <div className="col-md-6 col-12 wizard box-shadow d-flex">
                     <div className="main col-md-8">
@@ -152,31 +117,9 @@ function Create(){
                             { stepNumber === 1 && <div>
                                 <h3 className="ms-4 mb-4">Subjects</h3>
                                 <div className="row mx-0" id="subject">
-                                    { subjectsSelected.map((subject, index) => 
-                                            <div className={subject.isSelected? 'pill-active': 'pill'} onClick={() => handleSubjectClick(index)} onDoubleClick={()=> setPriorityShow(true)}>
+                                    { subjects.map((subject, index) => 
+                                            <div className={subject.isSelected? 'pill-active': 'pill'} onClick={() => {handleSubjectClick(index); !subject.isSelected && setPriorityShow(true);}}>
                                                 {subject.name}
-                                            </div>
-                                        )}
-                                </div>
-                            </div> }
-
-                            { stepNumber === 2 && <div>
-                                <h3 className="ms-4 mb-4">Teachers & Faculty</h3>
-                                <div className="row mx-0">
-                                    { teachersSelected.map((teacher, index) => 
-                                            <div className={teacher.isSelected? 'pill-active': 'pill'} onClick={() => handleTeacherClick(index)}>
-                                                {teacher.name}
-                                            </div>
-                                        )}
-                                </div>
-                            </div> }
-
-                            { stepNumber === 3 && <div>
-                                <h3 className="ms-4 mb-4">Rooms & Slots</h3>
-                                <div className="row mx-0">
-                                    { teachersSelected.map((teacher, index) => 
-                                            <div className={teacher.isSelected? 'pill-active': 'pill'} onClick={() => handleTeacherClick(index)}>
-                                                {teacher.name}
                                             </div>
                                         )}
                                 </div>
@@ -189,8 +132,8 @@ function Create(){
                     </div>
                     <div className="navigation">
                         { stepNumber !== 0 && <button className="custom-btn me-3 text-black-50" onClick={() => setStepNumber(stepNumber - 1)}>Previous</button>}
-                        { stepNumber !== 3 && <button className="custom-btn background-blue" onClick={() => setStepNumber(stepNumber + 1)} >Next</button>}
-                        { stepNumber === 3 && <Link to="/admin"><button className="custom-btn background-blue">Finish</button></Link>}
+                        { stepNumber !== 2 && <button className="custom-btn background-blue" onClick={() => setStepNumber(stepNumber + 1)} >Next</button>}
+                        { stepNumber === 2 && <Link to="/admin"><button className="custom-btn background-blue">Finish</button></Link>}
                     </div>
                 </div>
             </div>
